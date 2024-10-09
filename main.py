2d5c7089ecc809b45c982985118a36a3bc0024fa
Chetty-Praneeth <praneeth.chetty.12345@gmail.com>
2024-10-09 19:52:10 +0530

    Labels

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ from keras.models import load_model
 import cv2
 WINDOWSIZEX = 640
 WINDOWSIZEY = 480
+
+WHITE= (255,255,255)
+BLACK=(0,0,0)
+RED= (255,0,0)
+
+IMGAVE=False
+MODEL= load_model("my_model.h5")
+LABELS={0:"ZERP",1:"ONE",2:"TWO",3:"THREE",4:"FOUR",5:"FIVE",6:"SIX",7:"SEVEN",8:"EIGHT",9:"NINE"}
 #initializing the game
 pygame.init()
 
-FONT= pygame.font.Font("freesansbold.tff" 18)
+#FONT= pygame.font.Font("freesansbold.tff", 18)
 pygame.display.set_mode((WINDOWSIZEX, WINDOWSIZEY))
+
+pygame.display.set_caption("Digit Board")
 
 while True:
     for event in pygame.event.get():
